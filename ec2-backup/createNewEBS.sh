828bafda4a7010827c23693e8ec98d757fed13a9
ehg <chris.blower@jumpwire.co.uk>
2013-05-31 16:26:18 +0100

    create and attach an EBS volume to a machine

--- ec2-backup/createNewEBS.sh
+++ ec2-backup/createNewEBS.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-while getopts m:s:z: o; do
+while getopts m:s:z:g:d: o; do
   case "$o" in
-    m) UNIT="$OPTARG";;
-    s) SIZE="$OPTARG";;
+    s) SERVICE="$OPTARG";;
+    m) MACHINE="$OPTARG";;
+    g) SIZE="$OPTARG";;
     z) ZONE="$OPTARG";;
+    d) DEVICE="$OPTARG";;
     :) echo -n >&2 "$OPTARG requires argument"
        exit 1;;
-    [?])  echo >&2 "Usage: $0 <-m juju-unit> <-s size in GB> <-z region and zone>"
+    [?])  echo >&2 "Usage: $0 <-m juju-machine-id> <-s juju-service> <-g size in GB> <-z region and zone> <-d device>"
           exit 1;;
    esac
 done
 
-NEW_VOLUME_ID=$(ec2-create-volume --size $SIZE --availability-zone $ZONE | awk '{print $2}')
+CURRENT_VOLUMES=$(ec2-describe-volumes --show-empty-fields | grep TAG | grep "$SERVICE")
+if [ "$?" = "1" ]; then
+  echo >&2 "Service not found"
+  exit 1
+fi
 
-ec2-create-tags $NEW_VOLUME_ID -1a2b3c4d --tag "Name=$VOLUME_NAME"
+IFS='\n'
+for volume in $CURRENT_VOLUMES
+do
+  max_id=$(echo $volume | awk -F'\t' '{ print $5 }' | awk -F: '{print $2}' | sort -n | tail -1)
+done
+IFS=' '
+
+# Create new volume, give it a name of service:n
+NEW_VOLUME_ID=$(ec2-create-volume --size $SIZE --availability-zone $ZONE | awk '{print $2}')
+TAG_RESULT=$(ec2-create-tags $NEW_VOLUME_ID --tag "Name=${SERVICE}:$(($max_id + 1))")
+
+
+# Attach the volume to the machine
+ATTACH_TO_INSTANCE=$(./getInstanceByMachine.coffee $MACHINE)
+ec2-attach-volume $NEW_VOLUME_ID -i $ATTACH_TO_INSTANCE -d $DEVICE
