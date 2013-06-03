0fb31bb79f43caf21f675aac6838046328bc67f4
David Jones <drj@pobox.com>
2013-06-03 15:06:00 +0100

    Try and remove a few more bash-isms from the script.

--- ec2-backup/copySnapshots.sh
+++ ec2-backup/copySnapshots.sh
@@ -16,15 +16,14 @@ done
 SOURCE_SNAPSHOTS=$(ec2-describe-snapshots --region $SOURCE_REGION --show-empty-fields --filter status=completed| grep TAG | grep -v "\-COPIED")
 TARGET_SNAPSHOTS=$(ec2-describe-snapshots --region $TARGET_REGION | grep TAG )
 
-IFS=$'\n'
-for snapshot in $SOURCE_SNAPSHOTS
+printf '%s\n' "$SOURCE_SNAPSHOTS" |
+while read snapshot
 do
-  SNAPSHOT_ID=$(echo $snapshot | awk -F'\t' '{ print $3 }')
-  SNAPSHOT_NAME=$(echo $snapshot | awk -F'\t' '{ print $5 }')
+  SNAPSHOT_ID=$(echo $snapshot | awk '{ print $3 }')
+  SNAPSHOT_NAME=$(echo $snapshot | awk '{ print $5 }')
 
-  if ! echo $TARGET_SNAPSHOTS | grep -q "$SNAPSHOT_NAME"; then
+  if ! echo "$TARGET_SNAPSHOTS" | grep -q "$SNAPSHOT_NAME"; then
     SNAPSHOT_ID=$(ec2-copy-snapshot --source-region $SOURCE_REGION --source-snapshot-id $SNAPSHOT_ID --region $TARGET_REGION | awk -F'\t' '{print $2}')
     ec2-create-tags $SNAPSHOT_ID --region $TARGET_REGION --tag "Name=${SNAPSHOT_NAME}"
   fi
 done
-unset IFS
