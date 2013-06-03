e4ab224a91a305e35ac21c3a4106acc9ee2ccc34
ehg <chris.blower@jumpwire.co.uk>
2013-06-03 11:25:58 +0100

    give snapshots name tags instead of descriptions; only snapshot attached volumes

--- ec2-backup/snapshotVolumes.sh
+++ ec2-backup/snapshotVolumes.sh
@@ -5,13 +5,12 @@ while getopts n: o; do
     n) SEARCH="$OPTARG";;
     :) echo -n >&2 "$OPTARG requires argument"
        exit 1;;
-    [?])  echo >&2 "Usage: $0 -n <partial volume name>"
+    [?])  echo >&2 "Usage: $0 -n <partial volume name e.g. 'live'>"
           exit 1;;
    esac
 done
 
-
-VOLUMES=$(ec2-describe-volumes --show-empty-fields | grep TAG | grep "$SEARCH")
+VOLUMES=$(ec2-describe-volumes --show-empty-fields --filter attachment.status=attached | grep TAG | grep "$SEARCH")
 if [[ ("$SEARCH" = "") || ("$?" = "1") ]]; then
   echo >&2 "No volumes matching '$SEARCH' found"
   exit 1
@@ -24,6 +23,10 @@ do
   DATE=$(date "+%Y%m%dT%H%M%S")
   VOLUME_ID=$(echo $volume | awk -F'\t' '{ print $3 }')
   VOLUME_NAME=$(echo $volume | awk -F'\t' '{ print $5 }')
-  ec2-create-snapshot -d ${VOLUME_NAME}-${DATE} $VOLUME_ID
+  #TODO: set owner and restorable permissions to different accounts
+  SNAPSHOT_ID=$(ec2-create-snapshot $VOLUME_ID | awk -F'\t' '{print $2}')
+  ec2-create-tags $SNAPSHOT_ID --tag "Name=${VOLUME_NAME}:${DATE}"
 done
 unset IFS
+
+# TODO: Delete snapshots older than date?
